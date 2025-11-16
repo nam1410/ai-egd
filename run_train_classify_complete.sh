32a56b9da7bc616da4de05f3f7f67c006e0eebd5
nam1410 <namitha.guruprasad1410@gmail.com>
2025-11-16 13:15:07 -0500

    script for running training for higher number of epochs run_train_classifier_complete.sh

---
+++ run_train_classify_complete.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#SBATCH --job-name=dinov2_endo_train
+#SBATCH --output=slurm-%j.out
+#SBATCH --error=slurm-%j.err
+#SBATCH --time=06:00:00              # Adjust based on your needs
+#SBATCH --mem=16G                    # Memory per node
+#SBATCH --cpus-per-task=4            # CPU cores for data loading
+#SBATCH --gres=gpu:1                 # Request 1 GPU
+
+
+module --force purge
+# Load any necessary modules (if required by your cluster)
+module load StdEnv/2023 python/3.11.5 gcc/12.3 cuda/12.6 opencv/4.12
+
+
+# Activate virtual environment
+source ~/venvs/ai_egd/bin/activate
+
+
+# Print job info
+echo "Job ID: $SLURM_JOB_ID"
+echo "Node: $SLURM_NODELIST"
+echo "Start time: $(date)"
+echo "Working directory: $(pwd)"
+
+# Check GPU availability
+echo "GPU info:"
+nvidia-smi
+
+# Run training
+python -m src.train_classify \
+    --data_root /lustre06/project/6103394/ofarooq/AIEGD_datasets/ \
+    --patient_config_json /lustre06/project/6103394/ofarooq/ai-egd/src/datasets/patient_config.json \
+    --batch_size 8 \
+    --epochs_stage1 70 \
+    --epochs_stage2 30 \
+    --num_workers 4 \
+    --save_dir ./test_checkpoints
+
+echo "End time: $(date)"
+echo "Job completed with exit code: $?"
