1efef47650d48afe07aca99692ea71fde86e0472
nam1410 <namitha.guruprasad1410@gmail.com>
2025-11-16 10:17:48 -0500

    create run script for endo_xml_classification.py to test the dataset loader script for errors

---
+++ src/datasets/run_endo_xml_classification.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=16G
+#SBATCH --output=slurm-%j.out   # optional: capture output
+
+# Load any necessary modules (if required by your cluster)
+module load StdEnv/2023 python/3.11.5 gcc/12.3 cuda/12.6 opencv/4.12
+
+# Activate virtual environment
+source ~/venvs/ai_egd/bin/activate
+
+
+# Check GPU visibility (for debugging)
+nvidia-smi
+
+# to check if there are any errors in the script
+python endo_xml_classification.py
